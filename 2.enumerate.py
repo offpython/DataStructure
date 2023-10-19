818c9a47ac4e74ae8c708a75d0cbab1b855ed31a
offpython <offpython@gmail.com>
2023-10-19 19:18:42 +0900

    list

--- 2.enumerate.py
+++ 2.enumerate.py
@@ -1,150 +1,28 @@
-# 리스트
-name = '홍길동'
-print(f'name : {name}')
+# enumerate
 
-students = ['홍길동', '박찬호', '이용규', '박승철', '김지은']
-print(f'students : {students}')
+sports = ['농구', '축구', '야구', '마라톤', '테니스']
+for i in range(len(sports)):
+    print(f'{i} : {sports[i]}')
+for idx, value in enumerate(sports):
+    print(f'{idx} : {value}')
 
-numbers = [10, 20, 30, 40, 50, 60, 70]
-print(f'numbers : {numbers}')
+str = 'Hello Future'
+for idx, value in enumerate(str):
+    print(f'{idx} : {value}')
 
-str = [2.34, '십', 20, 'one', '3.141592']
-print(f'str : {str}')
+sports = ['농구', '수구', '축구', '마라톤', '테니스']
+favoriteSport = input('가장 좋아하는 스포츠 입력 : ')
 
-# 리스트 아이템 조회
-print(students[0])
-print(type(students))
-print(type(students[0]))
+bestSportIdx = 0
+for idx, value in enumerate(sports):
+    if value == favoriteSport:
+        bestSportIdx = idx + 1
 
-for i in range(5):
-    if i % 2 == 0:
-        print('인덱스가 짝수 -> students[{}] : {}'.format(i, students[i]))
-    else:
-        print('인덱스가 홀수 -> students[{}] : {}'.format(i, students[i]))
+print(f'{favoriteSport}는 {bestSportIdx}번 째에 있습니다.')
 
-# 리스트 길이
-sLength = len(students)
-print('length of students : {}'.format(sLength))
-
-str = 'hello python!!'
-print(len(str))
-
-for i in range(len(students)):
-    print(students[i])
-
-sports = ['수영', '배구', '농구', '조깅']
-n = 0
-while n < len(sports):
-    print(sports[n])
-    n += 1
-
-# 리스트와 for문
-cars = ['그랜저', '소나타', '말리부', '카니발', '쏘렌토']
-for car in cars:
-    print(car)
-
-studentCnts = [[1, 19], [2, 20], [3, 22], [4, 18], [5, 21]]
-sum = 0
-avg = 0
-for classNo, cnt in studentCnts:
-    print(f'{classNo}반 학생수 : {cnt}명')
-    sum += cnt
-    avg = sum/len(studentCnts)
-print(f'전체 학생 수 : {sum}명')
-print(f'평균 학생 수 : {avg}명')
-
-minScore = 60
-scores = [
-    ['국어', 58],
-    ['영어', 77],
-    ['수학', 89],
-    ['과학', 99],
-    ['국사', 50]]
-
-for score in scores:
-    if score[1] < minScore:
-        print(f'과락 과목 : {score[0]} -> 점수 : {score[1]}')
-
-for subject, score in scores:
-    if score < minScore:
-        print(f'과락 과목 : {subject} -> 점수 : {score}')
-
-for subject, score in scores:
-    if score >= minScore: continue
-    print(f'과락 과목 : {subject} -> 점수 : {score}')
-
-# 리스트와 while문
-cars = ['그랜저', '소나타', '말리부', '카니발', '쏘렌토']
-
-n = 0
-while n < len(cars):
-    print(cars[n])
-    n += 1
-n= 0
-flag = True
-while flag:
-    print(cars[n])
-    n += 1
-    if n == len(cars):
-        flag = False
-n = 0
-while True:
-    print(cars[n])
-    n += 1
-    if n == len(cars):
-        break
-
-studentCnts = [1, 18], [2, 19], [3, 23], [4, 21], [5, 20], [6, 22], [7, 17]
-sum = 0
-avg = 0
-n = 0
-while n < len(studentCnts):
-    classNo = studentCnts[n][0]
-    cnt = studentCnts[n][1]
-    print(f'{classNo}반 학생 수 : {cnt}')
-    sum += cnt
-    n += 1
-
-print(f'전체 학생 수 : {sum}명')
-print(f'평균 학생 수 : {sum/len(studentCnts)}명')
-
-minScore = 60
-scores = [
-    ['국어', 58],
-    ['영어', 77],
-    ['수학', 89],
-    ['과학', 99],
-    ['국사', 50]]
-
-n = 0
-while n < len(scores):
-    if scores[n][1] < minScore:
-        print(f'과락 과목 : {scores[n][0]}, {scores[n][1]}점')
-    n += 1
-
-n = 0
-while n < len(scores):
-    if scores[n][1] >= minScore:
-        n += 1
-        continue
-    print(f'과락 과목 : {scores[n][0]}, {scores[n][1]}점')
-    n += 1
-
-studentCnts = [1, 18], [2, 19], [3, 23], [4, 21], [5, 20], [6, 22], [7, 17]
-
-minClassNo = 0; maxClassNo = 0;
-minCnt = 0; maxCnt = 0;
-
-n = 0
-while n < len(studentCnts):
-    if minCnt == 0 or minCnt > studentCnts[n][1]:
-        minClassNo = studentCnts[n][0]
-        minCnt = studentCnts[n][1]
-    if maxCnt < studentCnts[n][1]:
-        maxClassNo = studentCnts[n][0]
-        maxCnt = studentCnts[n][1]
-    n += 1
-print(f'학생 수가 가장 적은 반 : {minClassNo}반, {minCnt}명')
-print(f'학생 수가 가장 적은 반 : {maxClassNo}반, {maxCnt}명')
-
-
+message = input('메세지 입력 : ')
+cnt = 0
+for idx, value in enumerate(message):
+    if value == ' ':
+        cnt += 1
+print(f'공백 갯수 : {cnt}')
--- 3.list.py
+++ 3.list.py
@@ -1,28 +1,212 @@
-# enumerate
+# # 리스트 특정 위치에 아이템 추가
+# students = ['홍길동', '박찬호', '이용규', '박승철', '김지은']
+# students.insert(3, '강호동')
+# print(students)
+#
+# words = ['I', 'a', 'Boy.']
+# for word in words:
+#     print(word, end = '')
+# print()
+#
+# words.insert(1, 'am')
+# for word in words:
+#     print(f'{word} ', end = '')
+# print()
+#
+# numbers = [1, 3, 6, 11, 45, 54, 62, 74, 85]
+# inputNum = int(input('숫자 입력 : '))
+# insertIdx = 0
+#
+# for idx, number in enumerate(numbers):
+#     print(idx, number)
+#     if insertIdx == 0 and inputNum < number:
+#         insertIdx = idx
+#
+# numbers.insert(insertIdx, inputNum)
+# print(numbers)
+#
+# # 리스트 아이템 삭제
+# students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+# students.pop()
+# print(students)
+# students.pop(2)
+# print(students)
+#
+# rValue = students.pop()
+# print(rValue)
+#
+# playerScore = [9.5, 8.9, 9.2, 9.8, 8.8, 9.0]
+# print(playerScore)
+# minScore = 0; maxScore = 0
+# minScoreIdx = 0; maxScoreIdx = 0
+#
+# for idx, score in enumerate(playerScore):
+#     if idx == 0 or minScore > score:
+#         minScoreIdx = idx
+#         minScore = score
+# playerScore.pop(minScoreIdx)
+#
+# for idx, score in enumerate(playerScore):
+#     if maxScore < score:
+#         maxScoreIdx = idx
+#         maxScore = score
+# playerScore.pop(maxScoreIdx)
+# print(playerScore)
+#
+# # 리스트 특정 아이템 삭제
+# students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+# students.remove('강호동')
+# print(students)
+#
+# students = ['홍길동', '박찬호', '이용규', '강호동', '박승철', '김지은', '강호동']
+# print(students)
+# while '강호동' in students:
+#     students.remove('강호동')
+# print(students)
+#
+# myList = ['마케팅 회의', '회의록 정리', '점심 약속', '월간 업무 보고', '치과 방문', '마트 장보기']
+# print(f'일정: {myList}')
+# removeItem = input('삭제 일정 입력 : ')
+# myList.remove(removeItem)
+# print(f'일정: {myList}')
+#
+# subjects = ['국어', '영어', '수학', '과학', '국사', '수학']
+# print(subjects)
+#
+# removeSubject = input('삭제 과목명 입력 : ')
+# while removeSubject in subjects:
+#     subjects.remove(removeSubject)
+# print(subjects)
 
-sports = ['농구', '축구', '야구', '마라톤', '테니스']
-for i in range(len(sports)):
-    print(f'{i} : {sports[i]}')
-for idx, value in enumerate(sports):
-    print(f'{idx} : {value}')
+# 리스트 연결
+group1 = ['홍길동', '박찬호', '이용규']
+group2 = ['강호동', '박승철', '김지은']
+group1.extend(group2)
+print(f'{group1}')
+print(f'{group2}')
 
-str = 'Hello Future'
-for idx, value in enumerate(str):
-    print(f'{idx} : {value}')
+result = group1 + group2
+print(result)
+print(f'{group1}')
+print(f'{group2}')
 
-sports = ['농구', '수구', '축구', '마라톤', '테니스']
-favoriteSport = input('가장 좋아하는 스포츠 입력 : ')
+myNum = [1, 3, 5, 6, 7]
+friendNum = [2, 3, 5, 8, 10]
+addList = myNum + friendNum
+print(addList)
 
-bestSportIdx = 0
-for idx, value in enumerate(sports):
-    if value == favoriteSport:
-        bestSportIdx = idx + 1
+result = []
+for number in addList:
+    if number not in result:
+        result.append(number)
+print(result)
 
-print(f'{favoriteSport}는 {bestSportIdx}번 째에 있습니다.')
+# 리스트 아이템 정렬
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+print(students)
+students.sort()
+print(students)
+students.sort(reverse=True)
+print(students)
 
-message = input('메세지 입력 : ')
-cnt = 0
-for idx, value in enumerate(message):
-    if value == ' ':
-        cnt += 1
-print(f'공백 갯수 : {cnt}')
+numbers = [2, 50, 4, 0.12, 3.14, 100]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
+
+playerScores = [9.5, 8.9, 9.2, 9.8, 8.8, 9.0]
+print(playerScores)
+
+playerScores.sort()
+print(playerScores)
+
+playerScores.pop(0)
+playerScores.pop(len(playerScores)-1)
+print(playerScores)
+
+sum = 0
+avg = 0
+for score in playerScores:
+    sum += score
+avg = sum / len(playerScores)
+print('sum : %.2f' % sum)
+print('avg : %.2f' % avg)
+
+# 리스트 아이템 순서 뒤집기
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+students.reverse()
+print(students)
+
+numbers = [9.5, 8.9, 9.2, 9.8, 8.8, 9.0]
+numbers.reverse()
+print(numbers)
+
+secret = '27156231'
+secretList = []
+solvedList = ''
+
+for cha in secret:
+    secretList.append(int(cha))
+
+print(secretList)
+secretList.reverse()
+print(secretList)
+
+val = secretList[0]*secretList[1]
+secretList.insert(2, val)
+
+val = secretList[3]*secretList[4]
+secretList.insert(5, val)
+
+val = secretList[6]*secretList[7]
+secretList.insert(8, val)
+
+val = secretList[6]*secretList[7]
+secretList.insert(8, val)
+
+val = secretList[9]*secretList[10]
+secretList.insert(11, val)
+
+print(secretList)
+
+# 리스트 슬라이스
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+print(students)
+print(students[2:])
+print(students[:4])
+print(students[:-2])
+print(students[4:-1])
+print(students[-5:-2])
+
+str = 'abcdefghijklmnopqrstuvwxyz'
+print(str)
+print(str[2:])
+print(str[:4])
+print(str[:-2])
+print(str[4:-1])
+print(str[-5:-2])
+
+numbers = [2, 50, 0.12, 1, 9, 7, 17, 35, 100, 3.14]
+print('numbers: {}'.format(numbers[2:-2]))
+print('numbers: {}'.format(numbers[2:-2:2]))
+print('numbers: {}'.format(numbers[:-2:2]))
+print('numbers: {}'.format(numbers[::2]))
+
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+print(students)
+students[1:4] = ['park', 'lee', 'ppark']
+print(students)
+
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+print(students)
+students[1:4] = ['park chanho', 'lee younggyu']
+print(students)
+
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+print('students: {}'.format(students))
+print('students: {}'.format(students[slice(2, 4)]))
+print('students: {}'.format(students[slice(4)]))
+print('students: {}'.format(students[slice(2, len(students))]))
+print('students: {}'.format(students[slice(2, len(students)-2)]))
+print('students: {}'.format(students[slice(len(students)-5, len(students)-2)]))
