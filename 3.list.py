14fc418b2ddd0f033654dba8396904154589731d
offpython <offpython@gmail.com>
2023-10-19 19:49:10 +0900

    list

--- 3.list.py
+++ 3.list.py
@@ -1,82 +1,82 @@
-# # 리스트 특정 위치에 아이템 추가
-# students = ['홍길동', '박찬호', '이용규', '박승철', '김지은']
-# students.insert(3, '강호동')
-# print(students)
-#
-# words = ['I', 'a', 'Boy.']
-# for word in words:
-#     print(word, end = '')
-# print()
-#
-# words.insert(1, 'am')
-# for word in words:
-#     print(f'{word} ', end = '')
-# print()
-#
-# numbers = [1, 3, 6, 11, 45, 54, 62, 74, 85]
-# inputNum = int(input('숫자 입력 : '))
-# insertIdx = 0
-#
-# for idx, number in enumerate(numbers):
-#     print(idx, number)
-#     if insertIdx == 0 and inputNum < number:
-#         insertIdx = idx
-#
-# numbers.insert(insertIdx, inputNum)
-# print(numbers)
-#
-# # 리스트 아이템 삭제
-# students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
-# students.pop()
-# print(students)
-# students.pop(2)
-# print(students)
-#
-# rValue = students.pop()
-# print(rValue)
-#
-# playerScore = [9.5, 8.9, 9.2, 9.8, 8.8, 9.0]
-# print(playerScore)
-# minScore = 0; maxScore = 0
-# minScoreIdx = 0; maxScoreIdx = 0
-#
-# for idx, score in enumerate(playerScore):
-#     if idx == 0 or minScore > score:
-#         minScoreIdx = idx
-#         minScore = score
-# playerScore.pop(minScoreIdx)
-#
-# for idx, score in enumerate(playerScore):
-#     if maxScore < score:
-#         maxScoreIdx = idx
-#         maxScore = score
-# playerScore.pop(maxScoreIdx)
-# print(playerScore)
-#
-# # 리스트 특정 아이템 삭제
-# students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
-# students.remove('강호동')
-# print(students)
-#
-# students = ['홍길동', '박찬호', '이용규', '강호동', '박승철', '김지은', '강호동']
-# print(students)
-# while '강호동' in students:
-#     students.remove('강호동')
-# print(students)
-#
-# myList = ['마케팅 회의', '회의록 정리', '점심 약속', '월간 업무 보고', '치과 방문', '마트 장보기']
-# print(f'일정: {myList}')
-# removeItem = input('삭제 일정 입력 : ')
-# myList.remove(removeItem)
-# print(f'일정: {myList}')
-#
-# subjects = ['국어', '영어', '수학', '과학', '국사', '수학']
-# print(subjects)
-#
-# removeSubject = input('삭제 과목명 입력 : ')
-# while removeSubject in subjects:
-#     subjects.remove(removeSubject)
-# print(subjects)
+# 리스트 특정 위치에 아이템 추가
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은']
+students.insert(3, '강호동')
+print(students)
+
+words = ['I', 'a', 'Boy.']
+for word in words:
+    print(word, end = '')
+print()
+
+words.insert(1, 'am')
+for word in words:
+    print(f'{word} ', end = '')
+print()
+
+numbers = [1, 3, 6, 11, 45, 54, 62, 74, 85]
+inputNum = int(input('숫자 입력 : '))
+insertIdx = 0
+
+for idx, number in enumerate(numbers):
+    print(idx, number)
+    if insertIdx == 0 and inputNum < number:
+        insertIdx = idx
+
+numbers.insert(insertIdx, inputNum)
+print(numbers)
+
+# 리스트 아이템 삭제
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+students.pop()
+print(students)
+students.pop(2)
+print(students)
+
+rValue = students.pop()
+print(rValue)
+
+playerScore = [9.5, 8.9, 9.2, 9.8, 8.8, 9.0]
+print(playerScore)
+minScore = 0; maxScore = 0
+minScoreIdx = 0; maxScoreIdx = 0
+
+for idx, score in enumerate(playerScore):
+    if idx == 0 or minScore > score:
+        minScoreIdx = idx
+        minScore = score
+playerScore.pop(minScoreIdx)
+
+for idx, score in enumerate(playerScore):
+    if maxScore < score:
+        maxScoreIdx = idx
+        maxScore = score
+playerScore.pop(maxScoreIdx)
+print(playerScore)
+
+# 리스트 특정 아이템 삭제
+students = ['홍길동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+students.remove('강호동')
+print(students)
+
+students = ['홍길동', '박찬호', '이용규', '강호동', '박승철', '김지은', '강호동']
+print(students)
+while '강호동' in students:
+    students.remove('강호동')
+print(students)
+
+myList = ['마케팅 회의', '회의록 정리', '점심 약속', '월간 업무 보고', '치과 방문', '마트 장보기']
+print(f'일정: {myList}')
+removeItem = input('삭제 일정 입력 : ')
+myList.remove(removeItem)
+print(f'일정: {myList}')
+
+subjects = ['국어', '영어', '수학', '과학', '국사', '수학']
+print(subjects)
+
+removeSubject = input('삭제 과목명 입력 : ')
+while removeSubject in subjects:
+    subjects.remove(removeSubject)
+print(subjects)
 
 # 리스트 연결
 group1 = ['홍길동', '박찬호', '이용규']
--- 4.list.py
+++ 4.list.py
@@ -1,1 +1,63 @@
-# 리스트 나머지 기능들
+# 리스트의 곱셈 연산
+students = ['홍길동', '박찬호', '이용구']
+print(students)
+mul = students*3
+print(mul)
+
+numbers = [2, 50, 0.12, 3, 9]
+print(numbers)
+mul = numbers * 2
+print(mul)
+
+# 아이템 위치 찾기
+students = ['홍길동', '강호동', '박찬호', '이용규', '박승철', '김지은', '강호동']
+searchIdx = students.index('강호동')
+searchIdx = students.index('강호동', 2, 7)
+print(searchIdx)
+
+import random
+
+sampleList = random.sample(range(1, 11), 10)
+selectIdx = int(input('숫자 7의 위치 입력 : '))
+searchIdx = sampleList.index(7)
+
+if searchIdx == selectIdx:
+    print('빙고!')
+else:
+    print('ㅜㅜ')
+
+print(sampleList)
+print(searchIdx)
+
+# 특정 아이템 개수 알아내기
+students = ['홍길동', '강호동', '박찬호', '이용규', '강호동', '박승철', '김지은']
+searchCnt = students.count('강호동')
+print(searchCnt)
+searchCnt = students.count('홍길동')
+print(searchCnt)
+searchCnt = students.count('나나')
+print(searchCnt)
+
+# 특정 아이템 삭제
+students = ['홍길동', '강호동', '박찬호', '이용규', '강호동', '박승철', '김지은']
+del students[0]
+print(students)
+
+del students[2]
+print(students)
+
+del students[3:]
+print(students)
+
+import random
+
+types = ['A', 'B', 'C', 'AB']
+todayData = []
+typeCnt = []
+
+for i in range(100):
+    type = types[random.randrange(len(types))]
+    todayData.append(type)
+
+for type in types:
+    print(f'{type}형 -> {todayData.count(type)}명')
